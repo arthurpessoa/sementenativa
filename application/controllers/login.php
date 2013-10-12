f545a7131764f4f019813074b2dae80a7e886249
Arthur Pessoa <arthur_pessoa@outlook.com>
2013-10-12 10:00:33 -0300

    adicionado logout

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,7 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-	
+	public function logout()
+	{
+		session_start();
+		session_destroy();
+	}
+
 	public function checklogin()
 	{
 	
