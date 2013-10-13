774acf3144387bccfda05614d141aac13e17a1f7
Arthur Pessoa <arthur_pessoa@outlook.com>
2013-10-12 23:20:26 -0300

    padronizacao do CP

--- application/views/controlPanel.php
+++ application/views/controlPanel.php
@@ -81,7 +81,7 @@
 										    </div>
 										    <div class="art-layout-cell layout-item-5" style="width: 25%" >
 										        <p style="text-align: center;"><a href=<?php echo base_url()."cadastrarColheita";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/produtores.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
-										        <h4 style="font-size:90%">Gerar documentos</h4>
+										        <h4 style="font-size:90%">Cadastrar Colheita</h4>
 										    </div>
 												
 									</div>
