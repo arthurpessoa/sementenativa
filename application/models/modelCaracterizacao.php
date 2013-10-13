ffaab52c29b1f941142b8ad474528662b59f6e88
Adriano Rodrigues <adrikei@gmail.com>
2013-10-13 15:40:15 -0300

    modelCaracterização.php
    talvez seja este model que chame a function dadoscaracterizacao, e não o
    ModelEspecie, como está.

---
+++ application/models/modelCaracterizacao.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class ModelCaracterizacao extends CI_Model {
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+    function addCaracterizacao($id, $dataColheita, $localOrigemSemente, $indicadorMaturacao, $dispersao, $frutosKilo, $pesoMilFrutos, $sementesPorFruto, $sementesPorKilo, $pesoMilSementes, $relacaoFrutoSemente, $tamanhoSemente, $tamanhoFruto, $tipoFruto, $pureza, $observacoes, $fontes)
+	{
+		$query = $this->db->query('INSERT INTO `sementeseviveiros`.`caracterizacao` (`idEspecie`,`dataColheita`,`localOrigemSemente`,`indicadorMaturacao`,`dispersao`,`frutosKilo`,`pesoMilFrutos`,`sementesPorFruto`,`sementesPorKilo`,`pesoMilSementes`,`relacaoFrutoSemente`,`tamanhoSemente`,`tamanhoFruto`,`tipoFruto`,`pureza`,`observacoes`,`fonres` ) VALUES (\''.$id.' \', \''.$dataColheita.' \', \''.$localOrigemSemente.' \', \''.$indicadorMaturacao.' \', \''.$dispersao.' \', \''.$frutosKilo.' \', \''.$pesoMilFrutos.' \', \''.$sementesPorFruto.' \', \''.$sementesPorKilo.' \', \''.$pesoMilSementes.' \', \''.$relacaoFrutoSemente.' \', \''.$tamanhoSemente.' \', \''.$tamanhoSemente.' \', \''.$tamanhoFruto.' \', \''.$tipoFruto.' \', \''.$pureza.' \', \''.$observações.' \', \''.$fontes.' \');');
+	}
+	//A que está sendo chamada não é essa, é em modelEspecie
+	function dadoscaracterizacao($id)
+	{
+		$query = $this->db->query('SELECT * FROM caracterizacao caracterizacao.idEspecie = '.$id);
+		return $query->result();
+	}
+}
