e8f0d149c063d1acd6e316564c8f055a9516c140
Adriano Rodrigues <adrikei@gmail.com>
2013-10-13 23:17:06 -0300

    be

--- application/views/especies.php
+++ application/views/especies.php
@@ -36,81 +36,11 @@
 			<div class="art-content-layout-row">
 				<div class="art-layout-cell art-content clearfix">
 					<article class="art-post art-article">
-						<h2 class="art-postheader">Cadastro de Usuário</h2>
-						<p>
-							<br>
-							<span id="erroPagina" class="erroinsert"><?php echo $erro ?></span>
-						</p>
+						<h2 class="art-postheader">Banco de espécies</h2>
 						<div class="art-postcontent art-postcontent-0 clearfix">
 							<div class="art-content-layout">
 								<div class="art-content-layout-row">
-									<div class="art-layout-cell layout-item-0" style="width: 130%" >
-											<form method="post" action=<?php echo base_url()."cadastrarUsuario/checkCadastro" ?>>
-											<p>
-												<label for="boxnome">Nome*</label>
-												<input id="boxnome" type="text" name="nome" size="40"/>
-												<span  hidden id="erroNomeVazio" class="erroinsert">*Não deve ser vazio</span>
-												<span  hidden id="erroNomeNumero" class="erroinsert">*Nome não deve conter números</span>
-												<span  hidden id="erroNomeGrande" class="erroinsert">*Nome deve conter no máximo 40 letras</span>
-											</p>
-											<p>
-												<label for="boxemail">Email*</label>
-												<input id="boxemail" type="text" name="email" size="40"/>
-												<span hidden id="erroEmail" class="erroinsert">*e-mail inválido</span>
-											</p>
-											<p>
-												<label for="boxsexo">Sexo</label>
-												<input id="sexo" type="radio" name="sexo" value="m" checked/>Masculino
-												<input id="sexo" type="radio" name="sexo" value="f"/>Feminino
-												<span id="erroSexo" class="erroinsert"></span>
-											</p>
-											<p>
-												<label for="boxcep">CEP</label>
-												<input name="cep" type="text" id="boxcep" value="" size="8" maxlength="8"/> (Sem traço)
-												<span hidden id="erroCepForma" class="erroinsert">*CEP não deve conter "-"</span>
-												<span hidden id="erroCepTamanho" class="erroinsert">*CEP deve conter 8 numeros</span>
-											</p>
-											<p>
-												<label for="boxend">Endereço</label>
-												<input id="boxend" name="endereco" type="text" size="40" />
-												<span hidden id="erroEnd" class="erroinsert">*Endereço não pode ser vazio</span>
-											</p>
-											<p>
-												<label for="boxnum">Número</label>
-												<input id="boxnum" name="num" type="text" size="5"/>
-												<span hidden id="erroNumVazio" class="erroinsert">*Numero não pode ser vazio</span>
-												<span hidden id="erroNumLetras" class="erroinsert">*Numero não pode conter letras</span>
-											</p>
-											<p>
-												<label for="boxbairro">Bairro</label>
-												<input id="boxbairro" name="bairro" type="text" id="bairro" size="30" />
-												<span hidden id="erroBairro" class="erroinsert">*Bairro não pode ser vazio</span>
-											</p>
-											<p>
-												<label for="boxcidade">Cidade</label>
-												<input id="boxcidade" name="cidade" type="text" size="40" />
-												<span hidden id="erroCidade" class="erroinsert">*Cidade não pode ser vazio</span>
-											</p>
-											<p>
-												<label for="boxuf">Estado</label>
-												<input id="boxuf" name="uf" type="text" size="2" />
-												<span hidden id="erroUF" class="erroinsert">*Estado não pode ser vazio</span>
-											</p>
-											<p>
-												<label for="boxsenha">Senha</label>
-												<input id="boxsenha" name="senha" type="password" size="12" />
-												<span hidden id="erroSenha" class="erroinsert">*Senha não pode ser vazia</span>
-											</p>
-											<p>
-												<label for="boxconfirmasenha">Confirmar Senha</label>
-												<input id="boxconfirmasenha" name="confirmasenha" type="password" size="12" />
-												<span hidden id="erroConfirmaSenha" class="erroinsert">*Confirmação diferente da senha digitada</span>
-											</p>
-											<br><br>
-											<p>
-												<p><a id="EnviarCadastro" href="#" style="float: right;" onclick="" class="art-button" >Cadastrar</a></p>
-											</p>
-										</form>		
+									<div class="art-layout-cell layout-item-0" style="width: 130%" >	
 									</div>
 								</div>
 							</div>
