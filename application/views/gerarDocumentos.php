4510b83ffa2d7b7dea450a87c8bbe0149bdd2499
Arthur Pessoa <arthur_pessoa@outlook.com>
2013-10-14 01:21:01 -0300

    bugs

---
+++ application/views/gerarDocumentos.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<?php session_start(); ?>
+<html dir="ltr" lang="en-US"><head><!-- Created by Artisteer v4.0.0.58833 -->
+    <meta charset="utf-8">
+    <title>Semente Nativa - Ajuda</title>
+    <meta name="viewport" content="initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no, width = device-width">
+
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    <link rel="stylesheet" href=<?php echo base_url()."assets/style.css"?> media="screen">
+    <!--[if lte IE 7]><link rel="stylesheet" href="style.ie7.css" media="screen" /><![endif]-->
+    <link rel="stylesheet" href=<?php echo base_url()."assets/style.responsive.css"?> media="all">
+
+
+    <script src=<?php echo base_url()."assets/jquery.js";?>></script>
+    <script src=<?php echo base_url()."assets/script.js"?>></script>
+    <script src=<?php echo base_url()."assets/script.responsive.js"?>></script>
+
+
+<style>.art-content .art-postcontent-0 .layout-item-0 { padding-right: 10px;padding-left: 10px;  }
+.ie7 .post .layout-cell {border:none !important; padding:0 !important; }
+.ie6 .post .layout-cell {border:none !important; padding:0 !important; }
+
+</style></head>
+<body>
+<div id="art-main">
+<nav class="art-nav clearfix">
+    <ul class="art-hmenu">
+           <li><a href=<?php echo base_url()."home";?>>Home</a></li>
+        <li><a href=<?php echo base_url()."sobre"?>               >Sobre</a></li>
+        <li><a href=<?php echo base_url()."contato"?>               >Contato</a></li>
+    </ul>
+</nav>
+<nav class="art-nav clearfix" style="   background: #6DAF28;">
+    <ul class="art-hmenu" style="height: 29px;">
+        <li><a href=<?php echo base_url()."controlPanel";?> >Painel de Controle</a></li>
+        <li><a href=<?php echo base_url()."gerarDocumentos";?> class="active" >Gerar Documentação</a></li>
+        <li><a href=<?php echo base_url()."ajuda";?> >Ajuda</a></li>
+    </ul>
+</nav>
+<header class="art-header clearfix">
+    <div class="art-shapes">
+<h1 class="art-headline" data-left="2.6%">
+        <a href=<?php echo base_url();?>>Sementes</a>
+</h1>
+<h2 class="art-slogan" data-left="2.45%">e Viveiros</h2>
+
+<div class="art-object0" data-left="100%"></div>
+
+            </div>
+                    
+</header>
+<div class="art-sheet clearfix">
+            <div class="art-layout-wrapper clearfix">
+                <div class="art-content-layout">
+                    <div class="art-content-layout-row">
+                        <div class="art-layout-cell art-content clearfix"><article class="art-post art-article">
+                                <h2 class="art-postheader">Gerar Documentos</h2>
+                                                
+                <div class="art-postcontent art-postcontent-0 clearfix"><div class="art-content-layout">
+    <div class="art-content-layout-row">
+    <div class="art-layout-cell layout-item-0" style="width: 100%" >
+        <p><span style="font-weight: bold;">
+
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam pharetra, tellus sit amet congue vulputate, nisi erat iaculis nibh, vitae feugiat sapien ante eget mauris. 
+
+Cras elit nisl, rhoncus nec iaculis ultricies, feugiat eget sapien. Pellentesque ac felis tellus. Aenean sollicitudin imperdiet arcu, vitae dignissim est posuere id. Duis placerat justo eu nunc interdum ultrices.
+
+        </p>
+    </div>
+    </div>
+</div>
+</div>
+</article></div>
+                    </div>
+                </div>
+            </div>
+    </div>
+<?php include 'modules/footer.php'; ?>
+</div>
+
+
+</body></html>
