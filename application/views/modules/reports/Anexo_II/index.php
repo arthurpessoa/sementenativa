aee99df652b2a3d16ee3238e96e11d141b9f8939
Arthur Pessoa <arthur_pessoa@outlook.com>
2013-11-04 14:03:38 -0200

    anexo I

---
+++ application/views/modules/reports/Anexo_II/index.php
@@ -0,0 +1,173 @@
+<html>
+<head>
+	<meta charset="utf-8" />
+    <title>Anexo II</title>
+    <style type="text/css" media="print">
+        @page 
+        {
+            size: auto;   /* auto is the current printer page size */
+            margin: 0mm;  /* this affects the margin in the printer settings */
+        }
+
+        body 
+        {
+            margin: 0px;  /* the margin on the content before printing */
+       	}
+    </style>
+</head>
+<body>
+
+	<style type="text/css" >    
+	.t {
+		position: absolute;
+		-webkit-transform-origin: top left;
+		-moz-transform-origin: top left;
+		-o-transform-origin: top left;
+		-ms-transform-origin: top left;
+		-webkit-transform: scale(0.25);
+		-moz-transform: scale(0.25);
+		-o-transform: scale(0.25);
+		-ms-transform: scale(0.25);
+		z-index: 1;
+		position:absolute;
+		white-space:nowrap;
+		overflow:visible;
+	}
+
+	#t1_1{left:610px;top:107px;}
+	#t2_1{left:226px;top:138px;}
+	#t3_1{left:74px;top:202px;}
+	#t4_1{left:797px;top:202px;}
+	#t5_1{left:74px;top:267px;}
+	#t6_1{left:130px;top:376px;}
+	#t7_1{left:267px;top:362px;}
+	#t8_1{left:293px;top:391px;}
+	#t9_1{left:409px;top:376px;}
+	#ta_1{left:506px;top:333px;}
+	#tb_1{left:521px;top:362px;}
+	#tc_1{left:507px;top:391px;}
+	#td_1{left:500px;top:420px;}
+	#te_1{left:708px;top:332px;}
+	#tf_1{left:1143px;top:362px;}
+	#tg_1{left:1119px;top:391px;}
+	#th_1{left:592px;top:391px;}
+	#ti_1{left:748px;top:362px;}
+	#tj_1{left:954px;top:376px;}
+	#tk_1{left:953px;top:406px;}
+	#tl_1{left:1043px;top:376px;}
+	#tm_1{left:1037px;top:406px;}
+	#tn_1{left:677px;top:406px;}
+	#to_1{left:778px;top:391px;}
+	#tp_1{left:775px;top:420px;}
+	#tq_1{left:854px;top:406px;}
+	#tr_1{left:74px;top:599px;}
+	#ts_1{left:74px;top:631px;}
+	#tt_1{left:74px;top:663px;}
+	#tu_1{left:209px;top:728px;}
+
+	.s1_1{
+		FONT-SIZE: 73px;
+		FONT-FAMILY: 'Times New Roman', Times, serif;
+		color: rgb(0,0,0);
+	}
+
+	.s2_1{
+		FONT-SIZE: 67px;
+		FONT-FAMILY: 'Times New Roman', Times, serif;
+		color: rgb(0,0,0);
+	}
+
+	</style>
+	<!-- End inline CSS -->
+
+	<!-- Begin embedded font definitions -->
+	<style type="text/css" >
+
+	@font-face {
+		font-family: TimesNewRoman1;
+		src: url("assets/fonts/TimesNewRoman.eot");
+		src: url("assets/fonts/TimesNewRoman.eot?#iefix") format("embedded-opentype"),
+			url("assets/fonts/TimesNewRoman.otf") format("opentype");
+	}
+
+	</style>
+
+	<!-- End embedded font definitions -->
+
+	<!-- Begin text definitions (Positioned/styled in CSS) -->
+	<div style="width:100%">
+	<div id="t1_1" class="t s1_1">Anexo II </div>
+	<div id="t2_1" class="t s1_1">RELATÓRIO ANUAL DE PRODUÇÃO E COMERCIALIZAÇÃO DE MUDAS DE ESPÉCIES FLORESTAIS </div>
+	<div id="t3_1" class="t s1_1">Produtor: </div>
+	<div id="t4_1" class="t s1_1">Inscrição no RENASEM nº: </div>
+	<div id="t5_1" class="t s1_1">Ano de produção: </div>
+	<div id="t6_1" class="t s2_1">Espécie </div>
+	<div id="t7_1" class="t s2_1">Nome Comum / </div>
+	<div id="t8_1" class="t s2_1">Cultivar </div>
+	<div id="t9_1" class="t s2_1">Categoria* </div>
+	<div id="ta_1" class="t s2_1">Saldo do </div>
+	<div id="tb_1" class="t s2_1">Ano </div>
+	<div id="tc_1" class="t s2_1">Anterior </div>
+	<div id="td_1" class="t s2_1">(unidades) </div>
+	<div id="te_1" class="t s2_1">Produção Acumulada no Ano (unidades) </div>
+	<div id="tf_1" class="t s2_1">Saldo </div>
+	<div id="tg_1" class="t s2_1">(unidades)** </div>
+	<div id="th_1" class="t s2_1">Produção </div>
+	<div id="ti_1" class="t s2_1">Comercializada </div>
+	<div id="tj_1" class="t s2_1">Plantio </div>
+	<div id="tk_1" class="t s2_1">Próprio </div>
+	<div id="tl_1" class="t s2_1">Outros </div>
+	<div id="tm_1" class="t s2_1">Destinos </div>
+	<div id="tn_1" class="t s2_1">No Estado </div>
+	<div id="to_1" class="t s2_1">Outros </div>
+	<div id="tp_1" class="t s2_1">Estados </div>
+	<div id="tq_1" class="t s2_1">Exportado </div>
+
+
+	<div id="pg1Overlay" style="width:100%; height:100%; position:absolute; z-index:0; background-color:rgba(0,0,0,0); -webkit-user-select: none;"></div>
+	<div id="pg1"><img src="<?php echo base_url()."assets/reports/Anexo_II/1.png";?>" id="pdf1" style="width:1286px; height:439px; background-color:white;"/></div>
+	<!--Dados -->
+
+
+	<div style="margin-left: 64px; margin-right: 64px">
+		<table border=0 style="border-collapse:collapse; width: 1158px;">
+		<tr>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<td style=" border: 1px solid gray;">row 1, cell 1</td>
+		<tr>
+		</table>
+	<br>
+	* Saldo de Sementes = saldo de sementes do ano anterior + produção do ano – (comercializada + plantio próprio + outros destinos) 
+	<br>
+	<br>
+	<br>
+	<div>
+		<table border=0>
+			<tr>
+				<td class="t s1_1" style="position: relative; border: 1px solid gray;">Local e Data: _________________________________</td>
+			</tr>
+			<tr>
+				<td class="t s1_1" style="position: relative; border: 1px solid gray;">Assinatura do Produtor:______________________________</td>
+			</tr>
+		</table>
+	</div>
+	</div>
+
+	<!-- End text definitions -->
+	<!-- Begin page background -->
+
+
+	<div id="brand" style="margin:0 auto; width:94px; padding:5px; clear: both;">
+	</div>
+	</div>
+</body>
+</html>
