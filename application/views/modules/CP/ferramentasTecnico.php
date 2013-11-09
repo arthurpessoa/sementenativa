2da402d98dfb5c2df0ed662eaeac1dec4cead36a
Arthur Pessoa <arthur_pessoa@outlook.com>
2013-11-09 18:50:55 -0200

    adicionado imagens

--- application/views/modules/CP/ferramentasTecnico.php
+++ application/views/modules/CP/ferramentasTecnico.php
@@ -3,20 +3,20 @@
 
 <div class="art-content-layout-row">
     <div class="art-layout-cell layout-item-3" style="width: 25%" >
-        <p style="text-align: center;"><a href=<?php echo base_url()."cadastrarCaracterizacao";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/produtores.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
+        <p style="text-align: center;"><a href=<?php echo base_url()."cadastrarCaracterizacao";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/caracterizacao.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
         <h4 style="font-size:90%">Caracterização de espécies</h4>
     </div>
     <div class="art-layout-cell layout-item-4" style="width: 25%" >
-        <p style="text-align: center;"><a href=<?php echo base_url()."cadastrarGerminacao";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/produtores.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
+        <p style="text-align: center;"><a href=<?php echo base_url()."cadastrarGerminacao";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/germinacao.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
         <h4 style="font-size:90%">Cadastrar dados da Germinacao</h4>
     </div>
     <div class="art-layout-cell layout-item-2" style="width: 25%" >
-        <p style="text-align: center;"><a href=<?php echo base_url()."especies";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/produtores.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
+        <p style="text-align: center;"><a href=<?php echo base_url()."especies";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/especies.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
         <h4 style="font-size:90%">Buscar Espécies</h4>
     </div>
 
     <div class="art-layout-cell layout-item-5" style="width: 25%" >
-        <p style="text-align: center;"><a href=<?php echo base_url()."gerarDocumentos";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/produtores.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
+        <p style="text-align: center;"><a href=<?php echo base_url()."gerarDocumentos";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/report.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
         <h4 style="font-size:90%">Gerar Documentação</h4>
     </div>
  </div>
