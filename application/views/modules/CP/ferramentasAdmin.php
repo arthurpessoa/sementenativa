a05e04a33380abf90e9b6fb00dd761fce1882539
Arthur Pessoa <arthur_pessoa@outlook.com>
2013-11-09 18:58:17 -0200

    adicionado imagens

--- application/views/modules/CP/ferramentasAdmin.php
+++ application/views/modules/CP/ferramentasAdmin.php
@@ -2,7 +2,7 @@
 <br>
 <div class="art-content-layout-row">
     <div class="art-layout-cell layout-item-2" style="width: 25%" >
-        <p style="text-align: center;"><a href=<?php echo base_url()."cadastrarEspecie";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/produtores.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
+        <p style="text-align: center;"><a href=<?php echo base_url()."cadastrarEspecie";?>><img width="100" height="100" alt="" src=<?php echo base_url()."assets/images/especies.jpg"?> style="margin-top: 5px; margin-right: 0px; margin-bottom: 5px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px;" class=""></a></p>
         <h4 style="font-size:90%">Cadastrar Espécies</h4>
     </div>
     <div class="art-layout-cell layout-item-0" style="width: 25%" >
